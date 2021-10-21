971d08e469c109fb84f2f65ee187d40fa7844f54
Tvibe <tvibe58@163.com>
2021-10-21 17:39:11 +0800

    引入element-plus

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store)
+
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router)
+
+// ElementPlus default language is english, we can config it
+app.use(ElementPlus, {
+  locale: zhCn
+})
+app.mount('#app')
